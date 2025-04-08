35bdfb31ec7ca301c8855844599247f884366c54
Calizaya Gerardo <147120972+WoShi1400@users.noreply.github.com>
2025-04-08 06:50:58 -0300

    Update 17_numero_par_o_impar.py
    Correccion

--- Clase_2/17_numero_par_o_impar.py
+++ Clase_2/17_numero_par_o_impar.py
@@ -6,7 +6,7 @@ print("Este programa determina si el numero ingresado es par o impar")
 numero = int(input("Ingresa un numero: "))
 
 if numero % 2 == 0:
-    print("El numero dado es impar.")
-else:
     print("El numero dado es par.")
+else:
+    print("El numero dado es impar.")
 
